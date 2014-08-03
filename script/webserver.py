1df5106d764482929a98c17ae262bb58842f7ba6
Bemoliph <bemoliph@gmail.com>
2014-08-03 00:55:38 -0400

    Python goals shifted towards API only; removes jinja2 dependencies and front page.

--- script/webserver.py
+++ script/webserver.py
@@ -1,11 +1,9 @@
 import cherrypy
 import psycopg2
 
-from huntsite import huntsite
 from huntapi import huntapi
 
-root = huntsite.HuntSite()
-root.api = huntapi.HuntAPI()
+api = huntapi.HuntAPI()
 
 if __name__ == "__main__":
-	cherrypy.quickstart(root, config="cherrypy.cfg")
+	cherrypy.quickstart(api, "/api", config="cherrypy.cfg")
