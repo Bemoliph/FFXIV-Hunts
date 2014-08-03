8f61fe0fc8440454ac0c53499fa0aeca5d58addb
Bemoliph <bemoliph@gmail.com>
2014-08-03 00:11:09 -0400

    Removed remnants of old database config code.

--- script/webserver.py
+++ script/webserver.py
@@ -7,14 +7,5 @@ from huntapi import huntapi
 root = huntsite.HuntSite()
 root.api = huntapi.HuntAPI()
 
-def connect():
-	credentials = cherrypy.config.get("database")
-	print credentials
-	
-	pool = psycopg2.pool.ThreadedConnectionPool(1, 10, **credentials)
-	
-	return pool
-
 if __name__ == "__main__":
 	cherrypy.quickstart(root, config="cherrypy.cfg")
-	pool = connect()
